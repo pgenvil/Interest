272017c609e186285309dd517c4d974cb9e2b60f
BingchenLi <461484709@qq.com>
2015-11-12 05:33:36 +0800

    WSP Project -2015

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def about # NEW
+    flash[:notice] = "Testing the flash"
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+
+  end
+  def create
+    secure_params = params.require(:user).permit(:name, :email,
+                                                 :password, :password_confirmation)
+    @user = User.new(secure_params)
+    if @user.save
+      flash[:success] = "Welcome to the Twitter App!"    # NEW LINE
+      redirect_to @user   # NEW LINE
+      # Handle a successful save.
+    else
+      render 'new'     # NEW LINE
+    end
+  end
+end
