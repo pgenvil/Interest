f97847cc16def92ac882e06f205a6de8b512532c
BingchenLi <461484709@qq.com>
2015-11-15 06:32:46 +0800

    finish basic layout

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,6 @@
 class MicropostsController < ApplicationController
    before_action :logged_in_user, only: [:create, :destroy]
-
+    before_action :correct_user,   only: :destroy 
       def create
       	 secure_post = params.require(:micropost).permit(:content)
         @micropost = current_user.microposts.build(secure_post) 
@@ -20,8 +20,8 @@ class MicropostsController < ApplicationController
 
        private
 
-            def correct_user
-              @micropost = current_user.microposts.find_by(id: params[:id])
-              redirect_to root_url if @micropost.nil?
+      def correct_user
+     @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
             end
     end
