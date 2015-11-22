dda75be524c45659d58de2cf1f1f24ce79573ea6
BingchenLi <461484709@qq.com>
2015-11-23 02:52:18 +0800

    delete the error table

--- db/migrate/20151122141903_create_interest_tag_user_join_table.rb
+++
@@ -1,9 +0,0 @@
-class CreateInterestTagUserJoinTable < ActiveRecord::Migration
-  def change
-  	create_table :interest_tags_users, :id=>false do|t|
-  	    t.integer :user_id
-        t.integer :interest_tag_id
-        end 
-
-  end
-end
