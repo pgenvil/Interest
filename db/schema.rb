b008bef0c92b2de64bf4e9600661f8c0be150032
BingchenLi <461484709@qq.com>
2015-11-23 04:34:04 +0800

    finish geocoder

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122143726) do
+ActiveRecord::Schema.define(version: 20151122191007) do
 
   create_table "interest_tags", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20151122143726) do
   create_table "interest_tags_users", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "interest_tag_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "microposts", force: :cascade do |t|
