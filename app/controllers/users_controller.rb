478784bd7d9db3705c8c0da783e36b75d19fecc7
BingchenLi <461484709@qq.com>
2015-11-22 23:19:38 +0800

    change some interet layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
-                                        :following, :followers]
+                                        :following, :followers,:viewinterest]
 
   def show
     @user = User.find(params[:id])
@@ -13,7 +13,8 @@ before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
   end
 
   def viewinterest
-     
+    
+     @interest_tags = current_user.interest_tags
    end
 
   def update
@@ -45,6 +46,7 @@ before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
   def edit
     @user = User.find(params[:id])
   end
+
   def following
     @title = "Following"
     @user  = User.find(params[:id])
--- app/models/interest_tag.rb
+++ app/models/interest_tag.rb
@@ -1,3 +1,4 @@
 class InterestTag < ActiveRecord::Base
 	 has_and_belongs_to_many :users
+	 
 end
