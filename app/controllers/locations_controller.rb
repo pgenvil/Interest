e00076980809db685919cc6dc4cce8d3928dbb11
BingchenLi <461484709@qq.com>
2015-12-04 22:45:08 +0800

    reconstruct some method

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -11,22 +11,20 @@ class LocationsController < ApplicationController
   end
 end
 
-  # GET /locations/1
-  # GET /locations/1.json
+  
   def show
   end
 
-  # GET /locations/new
+ 
   def new
     @location = Location.new
   end
 
-  # GET /locations/1/edit
+  
   def edit
   end
 
-  # POST /locations
-  # POST /locations.json
+ 
   def create
     @location = Location.new(location_params)
 
@@ -41,8 +39,7 @@ end
     end
   end
 
-  # PATCH/PUT /locations/1
-  # PATCH/PUT /locations/1.json
+  
   def update
     respond_to do |format|
       if @location.update(location_params)
@@ -55,8 +52,7 @@ end
     end
   end
 
-  # DELETE /locations/1
-  # DELETE /locations/1.json
+ 
   def destroy
     @location.destroy
     respond_to do |format|
