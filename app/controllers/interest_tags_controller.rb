0228ed6ad60a0ac0a2d843d84b712c18634237da
BingchenLi <461484709@qq.com>
2015-12-06 20:52:37 +0800

    add the comment

--- app/controllers/interest_tags_controller.rb
+++ app/controllers/interest_tags_controller.rb
@@ -1,10 +1,27 @@
 class InterestTagsController < ApplicationController
+ before_action :logged_in_user, only: [:create,:destroy]
 
 
 def index
      @interest=InterestTag.all
 end
 
+def new
+      @interest=InterestTag.new
+ end
 
+def create
+	 secure_post = params.require(:interest_tag).permit(:name)
+   @interest= current_user.interest_tags.build(secure_post) 
+      if @interest.save
+      	flash[:success] = "interest created!"
+        redirect_to action: 'index', notice: 'interest was successfully created.'
+      end  
+end	
+
+      def correct_user
+     @interest = current_user.interest_tags.find_by(id: params[:id])
+      redirect_to root_url if @interest.nil?
+    end
 
 end	
